f9cf0eb7b959be36b05576a2991552eab29fa92c
geek <geek@geekdeMac-mini.lan>
2026-06-26 13:10:20 +0800

    fix: add try-catch for BigQuery init

--- lib/getRank/BigQueryClient.ts
+++ lib/getRank/BigQueryClient.ts
@@ -5,17 +5,21 @@ const serviceKey = process.env.GOOGLE_SERVICE_KEY
 let BigqueryClient: any = null
 
 if (serviceKey) {
-  const credential = JSON.parse(
-    Buffer.from(serviceKey, 'base64').toString().replace(/\n/g, ''),
-  )
+  try {
+    const credential = JSON.parse(
+      Buffer.from(serviceKey, 'base64').toString().replace(/\n/g, ''),
+    )
 
-  BigqueryClient = new BigQuery({
-    projectId: credential.project_id,
-    credentials: {
-      client_email: credential.client_email,
-      private_key: credential.private_key,
-    },
-  })
+    BigqueryClient = new BigQuery({
+      projectId: credential.project_id,
+      credentials: {
+        client_email: credential.client_email,
+        private_key: credential.private_key,
+      },
+    })
+  } catch (e) {
+    console.log('Failed to initialize BigQuery client:', e)
+  }
 }
 
 export { BigqueryClient }
